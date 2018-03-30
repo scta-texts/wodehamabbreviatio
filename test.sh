618cca2d87e3a7dbe64fb4d524fc3c75c9646780
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2018-03-30 09:57:13 -0400

    added travis file with script instructions

---
+++ test.sh
@@ -0,0 +1,17 @@
+for dir in */
+  do
+    for file in $dir/*
+      do
+        schema=$(grep -o "lbp-.*-1.0.0" $file)
+      if [[ $file = *"transcriptions.xml"* ]]; then
+        jing ~/Desktop/tdf.rng $file
+        echo $file
+      elif [[ $schema = *"diplomatic"* ]]; then
+        echo "checking diplomatic for $file"
+        jing ~/Desktop/diplomatic.rng $file
+      elif [[ $schema = *"critical"* ]]; then
+        echo "checking critical for $file"
+        jing ~/Desktop/critical.rng $file
+      fi
+    done
+  done
