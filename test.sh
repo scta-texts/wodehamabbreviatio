e6edca3ca48f45ab95fb117b4e2a0b4687fab0b6
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2018-03-30 10:37:50 -0400

    modified travis script

--- test.sh
+++ test.sh
@@ -1,18 +1,14 @@
-for dir in */
+for file in */*
   do
-    for file in $dir/*
-      do
-        schema=$(grep -o "lbp-.*-1.0.0" $file)
-      if [[ $file = *"transcriptions.xml"* ]]; then
-        jing ~/Desktop/tdf.rng $file
-        echo $file
-      elif [[ $schema = *"diplomatic"* ]]; then
-        echo "checking diplomatic for $file"
-        jing ~/Desktop/diplomatic.rng $file
-      elif [[ $schema = *"critical"* ]]; then
-        echo "checking critical for $file"
-        jing ~/Desktop/critical.rng $file
-      fi
-    done
-  done
-  #test change
+    schema=$(grep -o "lbp-.*-1.0.0" $file)
+  if [[ $file = *"transcriptions.xml"* ]]; then
+    jing ~/Desktop/tdf.rng $file
+    echo $file
+  elif [[ $schema = *"diplomatic"* ]]; then
+    echo "checking diplomatic for $file"
+    jing ~/Desktop/diplomatic.rng $file
+  elif [[ $schema = *"critical"* ]]; then
+    echo "checking critical for $file"
+    jing ~/Desktop/critical.rng $file
+  fi
+done
