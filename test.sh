4157be5bc795835bb4d26537e0133a4c6ce254fa
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2018-03-31 09:41:44 -0400

    added aristotle quotation identification

--- test.sh
+++ test.sh
@@ -3,12 +3,22 @@ for file in */*
     schema=$(grep -o "lbp-.*-1.0.0" $file)
   if [[ $file = *"transcriptions.xml"* ]]; then
     jing ~/Desktop/tdf.rng $file
+    if [[ $? != 0 ]]
+    $status=$?
+    fi
     echo $file
   elif [[ $schema = *"diplomatic"* ]]; then
     echo "checking diplomatic for $file"
     jing ~/Desktop/diplomatic.rng $file
+    if [[ $? != 0 ]]
+    $status=$?
+    fi
   elif [[ $schema = *"critical"* ]]; then
     echo "checking critical for $file"
     jing ~/Desktop/critical.rng $file
+    if [[ $? != 0 ]]
+    $status=$?
+    fi
   fi
 done
+exit $status
